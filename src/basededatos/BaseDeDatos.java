9d5061817bb0f73e681761dba2bec0ef7fafa2b2
jgarciaamor <jgarciaamor@atanasoff01.danielcastelao.org>
2016-05-19 12:09:38 +0200

    BaseDatos

---
+++ src/basededatos/BaseDeDatos.java
@@ -0,0 +1,13 @@
+
+package basededatos;
+
+public class BaseDeDatos {
+
+    public static void main(String[] args) {
+        Interfaz interfaz = new Interfaz();
+        interfaz.setVisible(true);
+        
+        //Tutorial Conexion de MySQL Workbench a Netbeans Jose Luis Perez Gonzalez
+    }
+    
+}
